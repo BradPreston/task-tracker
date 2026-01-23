7cc927a12f1ec0768043b3dbcba2ca6b17e45756
bradpreston <bap5393@gmail.com>
2026-01-23 09:18:13 -0500

    update task status show page

--- resources/js/pages/TaskStatuses/Show.vue
+++ resources/js/pages/TaskStatuses/Show.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
     import { Head } from '@inertiajs/vue3';
 
+    import { Button } from '@/components/ui/button';
     import AppLayout from '@/layouts/AppLayout.vue';
-    import { index} from '@/routes/status';
+    import { formatDate } from '@/lib/utils';
+    import { index, edit } from '@/routes/status';
     import { type BreadcrumbItem, type TaskStatus } from '@/types';
+
 
     const breadcrumbs: BreadcrumbItem[] = [
         {
@@ -27,7 +30,12 @@
                 class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
             >
                 <h1>Task Status</h1>
-                <p>{{ props.status.title }}</p>
+                <p><strong>Title:</strong> {{ props.status.title }}</p>
+                <p><strong>Created At:</strong> {{ formatDate(props.status.created_at) }}</p>
+                <p><strong>Updated At:</strong> {{ formatDate(props.status.updated_at) }}</p>
+                <Button as-child class="mt-4 w-fit">
+                    <a :href="edit(props.status.id).url">Edit</a>
+                </Button>
             </div>
         </AppLayout>
     </template>
