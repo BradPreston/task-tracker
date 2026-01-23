7b22c1ced5b0f19cbffdc3c0c7fc8e71259e4680
bradpreston <bap5393@gmail.com>
2026-01-23 09:21:03 -0500

    remove comment from task status edit page

--- resources/js/pages/TaskStatuses/Edit.vue
+++ resources/js/pages/TaskStatuses/Edit.vue
@@ -6,7 +6,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AppLayout from '@/layouts/AppLayout.vue';
-// import status from '@/routes/status';
 import { index } from '@/routes/status';
 import { type BreadcrumbItem, type TaskStatus } from '@/types';
 
